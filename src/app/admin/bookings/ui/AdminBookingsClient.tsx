5087896e06ab99242e0a33b59fb86e968536054d
saleemsiddique <saleemprogramacion04@gmail.com>
2025-11-23 09:27:39 +0100

    Update AdminBookingsClient.tsx

--- src/app/admin/bookings/ui/AdminBookingsClient.tsx
+++ src/app/admin/bookings/ui/AdminBookingsClient.tsx
@@ -1033,7 +1033,7 @@ export default function AdminBookingsClient() {
                                         </div>
                                         {onlyAdmin ? (
                                             <div className="text-[10px] text-neutral-700">
-                                                Reservation <br></br> by admin
+                                                Reservation <br> </br> by admin
                                             </div>
                                         ) : (
                                             busy && (
